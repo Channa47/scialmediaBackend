e477c15842e29dbf2063785c6565ad6cba94f891
Channa47 <103635080+Channa47@users.noreply.github.com>
2023-01-16 16:28:26 +0530

    Cors

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const Connection = require("./config/db")
 const user = require("./Routes/UserRoutes")
 const post = require("./Routes/PostRoutes")
 const AuthMiddleware = require("./Middlewares/AuthMiddleWare")
-
+var cors = require('cors')
 
 
 
 const app = express();
+app.use(cors({origin:"*"}))
 app.use(express.json())
 app.use("/users",user);
 app.use(AuthMiddleware);
